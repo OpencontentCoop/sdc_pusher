9473adb2cdf5d6af455b8610af97487b9331815a
Luca Realdi <luca.realdi@opencontent.it>
2023-05-17 21:58:31 +0200

    no message

--- SdcUserSerializer.php
+++ SdcUserSerializer.php
@@ -6,15 +6,28 @@ class SdcUserSerializer
 {
     public function serialize(User $user): array
     {
-        SensorSdcPusher::debug("Serialize user $user->id");
-        $data = [
-            'nome' =>  $user->firstName,
-            'cognome' =>  $user->lastName,
-            'cellulare' =>  $user->phone,
-            'telefono' =>  $user->phone,
-            'email' =>  strtolower($user->email),
-            'codice_fiscale' =>  strtoupper($user->fiscalCode),
-        ];
+        if ($user->id === null){
+            SensorSdcPusher::error("Serialize null user");
+            $anonymous = eZUser::fetch(eZUser::anonymousId());
+            $data = [
+                'nome' =>  '?',
+                'cognome' =>  '?',
+                'cellulare' =>  '?',
+                'telefono' =>  '?',
+                'email' =>  strtolower($anonymous->attribute('email')),
+                'codice_fiscale' =>  'XXXXXX00X00X000X',
+            ];
+        }else {
+            SensorSdcPusher::debug("Serialize user $user->id");
+            $data = [
+                'nome' => $user->firstName,
+                'cognome' => $user->lastName,
+                'cellulare' => $user->phone,
+                'telefono' => $user->phone,
+                'email' => strtolower($user->email),
+                'codice_fiscale' => strtoupper($user->fiscalCode),
+            ];
+        }
 
         if (SensorSdcPusher::isDevMode()) {
             $data['nome'] = 'Jonas';
